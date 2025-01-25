33ebeb2d98ed623f5f8107d40c902ed679ed790c
Al-Auqash <64571752+Al-Auqash@users.noreply.github.com>
2025-01-25 09:06:19 +0700

    init

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for littlelemon project.
+"""Littlelemon URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('restaurant.urls')),
+    path('auth/', include('djoser.urls')), 
+    path('token/', include('djoser.urls.authtoken')), 
+    path('api/', include('LittlelemonAPI.urls')),
 ]
